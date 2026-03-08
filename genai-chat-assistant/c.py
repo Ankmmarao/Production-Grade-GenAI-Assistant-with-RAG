d56768ae73733ac93b8269c10a22ca13a39d86a3
Ankmmarao <127002167+Ankmmarao@users.noreply.github.com>
2026-03-08 09:05:37 +0530

    Update c.py

--- genai-chat-assistant/c.py
+++ genai-chat-assistant/c.py
@@ -1,10 +1,11 @@
 import google.generativeai as genai
 
 # Add your Gemini API key
-genai.configure(api_key="AIzaSyCR75vobImYDFjdbosNV7rvhHMEt9AjDqg")
+genai.configure(api_key="")
 
 # List available models
 for model in genai.list_models():
     
     if "generateContent" in model.supported_generation_methods:
+
         print(model.name)
